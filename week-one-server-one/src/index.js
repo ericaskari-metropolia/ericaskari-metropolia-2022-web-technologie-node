f3e34ba3b6c9334d419eaf088884cbd2a5ded0cd
Eric Askari <ericaskari@gmail.com>
2022-10-26 11:46:27 +0300

    node16

--- week-one-server-one/src/index.js
+++ week-one-server-one/src/index.js
@@ -2,30 +2,30 @@
 
 const express = require('express');
 const renderWelcomePage = require('./templates/welcome');
-
+const { camelCase } = require('lodash');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(express.static('public'))
+console.log(`Foo Bar in camelCase is ${camelCase('Foo Bar')}`);
 
+app.use(express.static('public'));
 
 app.get('/pug', (req, res) => {
     const rendered = renderWelcomePage({
         name: 'Viewer'
-    })
-    res.send(rendered)
-})
+    });
+    res.send(rendered);
+});
 
-app.get("/catinfo", (req, res) => {
+app.get('/catinfo', (req, res) => {
     const cat = {
-        name: "Frank",
-        birthdate: "2010-12-25",
-        weight: 5,
+        name: 'Frank',
+        birthdate: '2010-12-25',
+        weight: 5
     };
     res.json(cat);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${ port }`)
-})
-
+    console.log(`Example app listening on port ${port}`);
+});
--- week-one-server-one/src/templates/welcome.js
+++ week-one-server-one/src/templates/welcome.js
@@ -1,12 +1,10 @@
-const pug = require("pug");
-const { resolve } = require("path");
+const pug = require('pug');
+const { resolve } = require('path');
 
-module.exports =  function renderWelcomePage({name}) {
+module.exports = function renderWelcomePage({ name }) {
     const compiledFunction = pug.compileFile(resolve(__dirname, 'welcome.pug'), {});
 
     return compiledFunction({
         name
-    })
-}
-
-
+    });
+};
