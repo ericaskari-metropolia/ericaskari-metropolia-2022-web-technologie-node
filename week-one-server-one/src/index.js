aed76c56d8a3da0cf9114d5d4774d6c32b46b2d9
Eric Askari <ericaskari@gmail.com>
2022-10-26 10:21:02 +0300

    init

---
+++ week-one-server-one/src/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const renderWelcomePage = require('./templates/welcome');
+
+const app = express();
+const port = 3000
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    const rendered = renderWelcomePage({
+        name: 'Viewer'
+    })
+    res.send(rendered)
+})
+
+app.get("/catinfo", (req, res) => {
+    const cat = {
+        name: "Frank",
+        birthdate: "2010-12-25",
+        weight: 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
---
+++ week-one-server-one/src/templates/welcome.js
@@ -0,0 +1,12 @@
+const pug = require("pug");
+const { resolve } = require("path");
+
+module.exports =  function renderWelcomePage({name}) {
+    const compiledFunction = pug.compileFile(resolve(__dirname, 'welcome.pug'), {});
+
+    return compiledFunction({
+        name
+    })
+}
+
+
